9fd94d7062bd68683b7d68c1b16bba9e3af93267
Ruthwesh123 <ruthu.akhil@gmail.com>
2024-06-07 16:32:28 +0530

    updated the code

--- Variables.sh
+++ Variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-Person1=Virat
-Person2=Anushka
+Person1=$1
+Person2=$2
+Person3=$3
 $Person1::"Hello Can we meet today"
 $Person2::"No, I do have Shooting"
+$Person3::"How are you guys"
